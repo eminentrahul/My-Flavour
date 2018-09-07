6fe56770be80ed780d94075d5d6e8ad08bc78bbf
Rahul Ravi Prakash <rrp.nitp@gmail.com>
2018-09-07 23:30:19 +0530

    Initial Commit

---
+++ My-Flavour/My-Flavour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My-Flavour
+//
+//  Created by Rahul Ravi Prakash on 07/09/18.
+//  Copyright © 2018 Rahul Ravi Prakash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
